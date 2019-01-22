86062d71e0e610eb10fa2cfa2794df6fd839b337
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-01-21 20:44:46 -0500

    Renamed build and test scripts

--- local_test.sh
+++ local_test.sh
@@ -3,9 +3,6 @@
 set -x
 
 export UNITY_EXECUTABLE=${UNITY_EXECUTABLE:-"/Applications/Unity/Hub/Editor/2018.2.6f1/Unity.app/Contents/MacOS/Unity"}
-export BUILD_NAME=${BUILD_NAME:-"ExampleProjectName"}
 
-BUILD_TARGET=StandaloneLinux64 ./ci/build.sh
-BUILD_TARGET=StandaloneOSX ./ci/build.sh
-BUILD_TARGET=StandaloneWindows64 ./ci/build.sh
-BUILD_TARGET=WebGL ./ci/build.sh
+TEST_PLATFORM=editmode ./ci/test.sh
+TEST_PLATFORM=playmode ./ci/test.sh
