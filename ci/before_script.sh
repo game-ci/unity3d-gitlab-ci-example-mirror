c4a2c433d7123101fca2f780dc6ac1885229c83e
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-08-13 19:34:05 -0400

    Apply workaround for webgl missing ffmpeg
    relates to https://gitlab.com/gableroux/unity3d-gitlab-ci-example/issues/36

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -8,4 +8,13 @@ set +x
 echo 'Writing $UNITY_LICENSE_CONTENT to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf'
 echo "$UNITY_LICENSE_CONTENT" | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
 
+set -x
+
+# Workaround for https://gitlab.com/gableroux/unity3d/issues/35
+# Prevents ALSA lib errors
 cp ci/etc/asound.conf /etc/asound.conf
+
+# Workaround for https://gitlab.com/gableroux/unity3d-gitlab-ci-example/issues/36
+# webgl build fails due to missing ffmpeg otherwise
+sudo apt-get update
+sudo apt-get install -y ffmpeg
