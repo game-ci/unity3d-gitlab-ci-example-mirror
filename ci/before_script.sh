7e8d63c91ece89f45834a8f61b566391c5d11a82
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-09-02 23:29:48 -0400

    Remove asound and ffmpeg workarounds (now bundled in base image)

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -10,10 +10,3 @@ echo "$UNITY_LICENSE_CONTENT" | tr -d '\r' > /root/.local/share/unity3d/Unity/Un
 
 set -x
 
-# Workaround for https://gitlab.com/gableroux/unity3d/issues/35
-# Prevents ALSA lib errors
-cp ci/etc/asound.conf /etc/asound.conf
-
-# Workaround for https://gitlab.com/gableroux/unity3d-gitlab-ci-example/issues/36
-# webgl build fails due to missing ffmpeg otherwise
-apt-get update && apt-get install -y ffmpeg
