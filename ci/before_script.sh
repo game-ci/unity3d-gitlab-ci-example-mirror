a45a53116277038a8349d3a7e56fa41499e8f385
Théo Sabattié <theo.sabattie@gmail.com>
2019-09-09 21:03:23 +0200

    Improve before_script.sh to use specific license if necessary

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -5,8 +5,18 @@ set -x
 mkdir -p /root/.cache/unity3d
 mkdir -p /root/.local/share/unity3d/Unity/
 set +x
-echo 'Writing $UNITY_LICENSE_CONTENT to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf'
-echo "$UNITY_LICENSE_CONTENT" | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
+
+LICENSE="UNITY_LICENSE_CONTENT_"${BUILD_TARGET^^}
+
+if [ -z "${!LICENSE}" ]
+then
+    echo "$LICENSE env var not found, using default UNITY_LICENSE_CONTENT env var"
+    LICENSE=UNITY_LICENSE_CONTENT
+else
+    echo "Using $LICENSE env var"
+fi
+
+echo "Writing $LICENSE to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf"
+echo "${!LICENSE}" | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
 
 set -x
-
