896c8caece844aa04811da1640e1ad9fb8f59aa5
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-12-13 15:46:11 -0500

    Fix build path in build script (Ruud Peter's suggestion, thanks)

--- ci/build.sh
+++ ci/build.sh
@@ -5,7 +5,7 @@ set -x
 
 echo "Building for $BUILD_TARGET"
 
-export BUILD_PATH=/project/Builds/$BUILD_TARGET/
+export BUILD_PATH=./Builds/$BUILD_TARGET/
 mkdir -p $BUILD_PATH
 
 ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
