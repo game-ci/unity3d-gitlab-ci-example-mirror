4b294b9b22cf786d6c93b707e0758428c00e8a0d
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2020-11-01 12:09:02 -0500

    Update local scripts to 2020.1.11f1

--- local_build.sh
+++ local_build.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-export UNITY_EXECUTABLE=${UNITY_EXECUTABLE:-"/Applications/Unity/Hub/Editor/2019.3.7f1/Unity.app/Contents/MacOS/Unity"}
+export UNITY_EXECUTABLE=${UNITY_EXECUTABLE:-"/Applications/Unity/Hub/Editor/2020.1.11f1/Unity.app/Contents/MacOS/Unity"}
 export BUILD_NAME=${BUILD_NAME:-"ExampleProjectName"}
 
 BUILD_TARGET=StandaloneLinux64 ./ci/build.sh
