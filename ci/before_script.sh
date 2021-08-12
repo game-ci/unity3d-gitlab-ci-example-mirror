f0db5688414e7bc5deccbb0d23d6ec056b379f78
Steven Shadow <huffy7412@gmail.com>
2021-08-12 01:17:55 +0000

    Replaced rogue tab with spaces, to match the rest of the file

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -11,7 +11,7 @@ UPPERCASE_BUILD_TARGET=${BUILD_TARGET^^};
 if [ $UPPERCASE_BUILD_TARGET = "ANDROID" ]
 then
     if [ -n $ANDROID_KEYSTORE_BASE64 ]
-	then
+    then
         echo '$ANDROID_KEYSTORE_BASE64 found, decoding content into keystore.keystore'
         echo $ANDROID_KEYSTORE_BASE64 | base64 --decode > keystore.keystore
     else
